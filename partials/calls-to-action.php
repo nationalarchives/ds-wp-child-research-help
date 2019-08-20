f176b14d60a46f4e20ef3766c4af4642d3500373
domingobishop <domingobishop@gmail.com>
2019-08-20 16:05:15 +0100

    find a guide and calls to action partials

---
+++ partials/calls-to-action.php
@@ -0,0 +1,36 @@
+<div class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="entry-content">
+                        <h3>Unfamiliar with archives?</h3>
+                        <p>
+                            <a href="#" class="button">Action button</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="entry-content">
+                        <h3>Search our records</h3>
+                        <p>
+                            <a href="#" class="button">Action button</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="entry-content">
+                        <h3>Need help using Discovery?</h3>
+                        <p>
+                            <a href="#" class="button">Action button</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ partials/find-a-research-guide.php
@@ -0,0 +1,51 @@
+<div class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h2>Find a research guide</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-family.jpg);"></div>
+                        <div class="entry-content">
+                            <h3>Family history</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-firstworldwar.jpg);"></div>
+                        <div class="entry-content">
+                            <h3>First World War</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-secondworldwar.jpg);"></div>
+                        <div class="entry-content">
+                            <h3>Second World War</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-militarymaritime.jpg);"></div>
+                        <div class="entry-content">
+                            <h3>Military and maritime</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
