6b6759cf2b829fa3a67f797b80c51cd9be1002fb
Punal Chotrani <punalsc@gmail.com>
2019-08-21 17:59:23 +0100

    Added new partial

---
+++ partials/how-to-view-a-record.php
@@ -0,0 +1,70 @@
+<div class="cards">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 ">
+                <div class="entry-header ">
+                    <h2>How to view our records</h2>
+                </div>
+                <div class="row cards ">
+                    <div class="col-md-8 ">
+                        <div class="card ">
+                            <a href="# ">
+                                <div class="entry-image "
+                                    style="background-image: url(http://www.nationalarchives.gov.uk/images/home/map-location-the-national-archives.jpg) ">
+                                </div>
+                                <div class="entry-content ">
+
+                                    <h3>Visit Us</h3>
+                                    <p>Visit Kew to see original documents or records. Entrance is free and there is no
+                                        need to
+                                        book.</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-md-4 ">
+                        <div class="card ">
+                            <a href="# ">
+                                <h3>Pay for research</h3>
+                            </a>
+                            <p>Use our paid search service or find an independent researcher.</p>
+                        </div>
+                        <div class="card ">
+                            <a href="# ">
+                                <h3>Order copies</h3>
+                            </a>
+                            <p>We can either <a href="http://www.nationalarchives.gov.uk/recordcopying ">copy our
+                                    records</a>
+                                onto paper or deliver them to you digitally.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 ">
+                <div class="entry-header hue-green card-spacing ">
+                    <h2>Contact us for advice</h2>
+
+                    <div class="still-need-help ">
+                        <h3>Still need help?</h3>
+                        <div class="card ">
+
+                            <a href="http://apps.nationalarchives.gov.uk/contact/#livechat ">
+                                <h4>Live chat</h4>
+                            </a>
+                            <p>For quick pointers.Tuesday to Saturday 09:00 to 17:00</p>
+                        </div>
+                        <div class="card ">
+                            <a href="# ">
+                            </a>
+                            <h4><a href="# "></a><a
+                                    href="http://apps.nationalarchives.gov.uk/contact/contactform.asp?id=22 "
+                                    title="Email us ">Email</a></h4>
+
+                            <p>For more detailed research enquiries.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
