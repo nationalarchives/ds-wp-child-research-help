567d73f65af35d4faada864ceb3424f7de19b701
Punal Chotrani <punalsc@gmail.com>
2019-08-21 16:36:25 +0100

    Added content to partials

--- partials/find-a-research-guide.php
+++ partials/find-a-research-guide.php
@@ -5,11 +5,14 @@
                 <h2>Find a research guide</h2>
             </div>
         </div>
+        <?php include 'hwyr-toolbar.php'; ?>
         <div class="row">
             <div class="col-md-3">
                 <div class="card">
                     <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-family.jpg);"></div>
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-family.jpg);">
+                        </div>
                         <div class="entry-content">
                             <h3>Family history</h3>
                         </div>
@@ -19,7 +22,9 @@
             <div class="col-md-3">
                 <div class="card">
                     <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-firstworldwar.jpg);"></div>
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-firstworldwar.jpg);">
+                        </div>
                         <div class="entry-content">
                             <h3>First World War</h3>
                         </div>
@@ -29,7 +34,9 @@
             <div class="col-md-3">
                 <div class="card">
                     <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-secondworldwar.jpg);"></div>
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-secondworldwar.jpg);">
+                        </div>
                         <div class="entry-content">
                             <h3>Second World War</h3>
                         </div>
@@ -39,9 +46,107 @@
             <div class="col-md-3">
                 <div class="card">
                     <a href="#">
-                        <div class="entry-image" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-militarymaritime.jpg);"></div>
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-militarymaritime.jpg);">
+                        </div>
                         <div class="entry-content">
                             <h3>Military and maritime</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-socialcultural.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Social and culture history</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-criminalscourtsprisons.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Criminal, courts and prisons</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-politicaleconomic.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Political and economic history</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-foreigncolonial.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Foreign and colonial history</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-landmaps.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Land and maps</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-medievalearlymodern.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Medieval and early modern history</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-currentlegal.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Records for current legal purposes</h3>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <a href="#">
+                        <div class="entry-image"
+                            style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/banner-category-onlinecollections.jpg);">
+                        </div>
+                        <div class="entry-content">
+                            <h3>Online collections</h3>
                         </div>
                     </a>
                 </div>
---
+++ partials/hwyr-toolbar.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col-md-12 tool-bar-col">
+        <div class="hue-green tool-bar">
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="/help-with-your-research/research-guides-keywords/" class="button default">Use our
+                        A-Z index</a> or select a subject below.
+                </div>
+                <div class="col-md-6">
+                    <div class="hwyr-form-wrapper">
+                        <form method="get" action="/help-with-your-research/research-guides/" role="search"
+                            class="input-extended position-relative pad-small margin-medium hue-25-black">
+                            <label for="Search">Search all research guides</label>
+                            <input type="text" id="search" name="search" placeholder="Search all research guides"
+                                value="">
+                            <input type="submit" class="search-button-medium" alt="Submit search" value="">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
