c24c21bd2b18441e429d2e84ee0eb14602da12a5
domingobishop <domingobishop@gmail.com>
2020-10-12 16:18:13 +0100

    aws proxy check

--- inc/functions-globals.php
+++ inc/functions-globals.php
@@ -20,6 +20,7 @@ function setThemeGlobals($environment = null) {
             $pre_crumbs = array('Help with your research' => '/');
             break;
         case 'external':
+        case 'aws_public':
             $pre_crumbs = array(
                 'Help with your research' => '/help-with-your-research/'
             );
